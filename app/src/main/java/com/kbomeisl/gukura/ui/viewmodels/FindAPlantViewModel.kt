35b0778e8d51412b14d9963ae200b37a2d9741ee
Kyle Bomeisl <bomeisl@scarletmail.rutgers.edu>
2024-10-18 04:56:16 -0400

    Add methods to retrieve plants by temperature, humidity, and sunlight level range from Gukura's online database

--- app/src/main/java/com/kbomeisl/gukura/ui/viewmodels/FindAPlantViewModel.kt
+++ app/src/main/java/com/kbomeisl/gukura/ui/viewmodels/FindAPlantViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.room.util.convertByteToUUID
 import com.kbomeisl.gukura.data.database.PlantDao
 import com.kbomeisl.gukura.data.database.toUi
 import com.kbomeisl.gukura.data.network.PlantNetwork
@@ -41,11 +42,48 @@ class FindAPlantViewModel(
         sensorDataSource.detatchListener()
     }
 
+    //Get plants by name from Gukura's online database
     fun getPlantFromFirebase(plantName: String): Deferred<PlantUi> {
         val plantNetworkJob = coroutineScope.async {
            findAPlantRepository.lookUpPlantApi(plantName = plantName).toUi()
         }
         return plantNetworkJob
+    }
+
+    //Get plants from Gukura's online database that would thrive at a given temperature
+    fun getPlantsFromFirebaseByTemperature(temperature: Float): Deferred<List<PlantUi>> {
+        val plantTempNetworkJob = coroutineScope.async {
+            findAPlantRepository.getPlantListApi().filter {
+                it.minTemperature < temperature && it.maxTemperature > temperature
+            }.map {
+                plantNetwork -> plantNetwork.toUi()
+            }
+        }
+        return plantTempNetworkJob
+    }
+
+    //Get plants from Gukura's online database that would thrive at a given humidity
+    fun getPlantsFromFirebaseByHumidity(humidity: Float): Deferred<List<PlantUi>> {
+        val plantHumidityNetworkJob = coroutineScope.async {
+            findAPlantRepository.getPlantListApi().filter {
+                it.minHumidity < humidity && it.maxHumidity > humidity
+            }.map {
+                plantNetwork -> plantNetwork.toUi()
+            }
+        }
+        return plantHumidityNetworkJob
+    }
+
+    //Get plants from Gukura's online database that would thrive at a given level of sunlight
+    fun getPlantsFromFirebaseByLightLevel(lightLevel: Float): Deferred<List<PlantUi>> {
+        val plantLightNetworkJob = coroutineScope.async {
+            findAPlantRepository.getPlantListApi().filter {
+                it.minLightLevel < lightLevel && it.maxLightLevel > lightLevel
+            }.map {
+                plantNetwork -> plantNetwork.toUi()
+            }
+        }
+        return plantLightNetworkJob
     }
 
     fun getPlantFromDatabase(plantName: String): PlantUi {
